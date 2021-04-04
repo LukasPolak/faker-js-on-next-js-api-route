640611ffcddb15140e8affb866cba78adbba3c2b
Lukas Polak <lukash.polak@gmail.com>
2021-04-04 15:26:16 +0200

    initialize customer api

--- pages/api/customers.tsx
+++ pages/api/customers.tsx
@@ -1,5 +1,15 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import faker from "faker";
 
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+  const limit = JSON.parse(req.body)?.limit ?? 10;
+
+  const customers = [...new Array(limit)].map((_, index) => {
+    return {
+      index,
+      name: faker.name.findName(),
+      city: faker.address.city(),
+    };
+  });
+
+  res.status(200).json(JSON.stringify(customers));
+};
