978ff27a4e9d47acf1d09f0b2ec88e88a11b3808
Lukas Polak <lukash.polak@gmail.com>
2021-04-04 15:26:22 +0200

    fetch api data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,29 @@
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return <h1>Hello, Vercel!</h1>
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("/api/customers", {
+        method: "POST",
+        body: JSON.stringify({
+          limit: 100,
+        }),
+      });
+
+      return response.json();
+    };
+
+    fetchData().then((data) => {
+      setData(data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Hello, Vercel!</h1>
+      {!data ? "...loading" : <pre>{JSON.stringify(data, null, 2)}</pre>}
+    </div>
+  );
 }
